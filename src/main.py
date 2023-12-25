66be5138e99ad3fa82be0b748b39b3686eb551ae
Lukas Keller <keller.lukas@bluewin.ch>
2023-12-25 15:57:25 +0100

    get list from stvadmin automatically

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import numpy as np
 from src.utils.databases import MailBasedDatabase, Database, Person
 from src.utils.cleverreach_database import CleverreachDatabase
 from src.utils.adress_databases import AdressDatabase
+from src.utils.dynamics_client import DynamicsClient
 
 
 def export_no_mail_people_excel(mb_db: MailBasedDatabase, output_file: str):
@@ -15,48 +16,74 @@ def export_no_mail_people_excel(mb_db: MailBasedDatabase, output_file: str):
     ad_db = AdressDatabase(input_db=no_mail_db)
     ad_db.to_excel(output_file)
 
+
 def load_main_db(input_file: str) -> Database:
     main_db = Database(input_file)
     return main_db
 
+
 def load_mail_based_db(main_db: Database) -> MailBasedDatabase:
     mb_db = MailBasedDatabase(input_db=main_db)
     return mb_db
+
 
 def add_from_additional_list(main_db: Database, input_file: str) -> Database:
     additional_db = Database(input_file)
     main_db.add_people(additional_db.people)
     return main_db
 
+
 def add_mail_from_backup_db(main_db: Database, input_file: str) -> Database:
     backup_db = Database(input_file)
-    main_db.copy_value_of_property_from_referencelist_if_empty_and_all_other_properties_match("email", backup_db.people)
+    main_db.copy_value_of_property_from_referencelist_if_empty_and_all_other_properties_match(
+        "email", backup_db.people
+    )
     return main_db
+
 
 def remove_mail_from_removelist(main_db: Database, input_file: str) -> Database:
     remove_from_mailinglist_db = Database(input_file)
-    remove_from_mailinglist_db_only_mail = [Person(emails=[person.email]) for person in remove_from_mailinglist_db.people]
-    main_db.remove_property_for_people_matching_removelist("email", remove_from_mailinglist_db_only_mail)
+    remove_from_mailinglist_db_only_mail = [
+        Person(emails=[person.email]) for person in remove_from_mailinglist_db.people
+    ]
+    main_db.remove_property_for_people_matching_removelist(
+        "email", remove_from_mailinglist_db_only_mail
+    )
     return main_db
+
 
 def export_as_cleverreach_csv(mb_db: MailBasedDatabase, output_file: str):
     cr_db = CleverreachDatabase(input_mb_database=mb_db)
     cr_db.to_csv(output_file)
-    
-def export_adult_people_joined_at_or_after_date_excel(main_db: Database, date: pd.Timestamp, output_file: str):
-    adult_cat = ["Aktive Turner", "Aktive Turnerin", "Passivmitglied", "Freimitg. Turnend (1)", "Freimitg. nturnend (10)", "Ehrenmitg. nturnend", "Ehrenmitg. turnend"]
+
+
+def export_adult_people_joined_at_or_after_date_excel(
+    main_db: Database, date: pd.Timestamp, output_file: str
+):
+    adult_cat = [
+        "Aktive Turner",
+        "Aktive Turnerin",
+        "Passivmitglied",
+        "Freimitg. Turnend (1)",
+        "Freimitg. nturnend (10)",
+        "Ehrenmitg. nturnend",
+        "Ehrenmitg. turnend",
+    ]
     adult_people = []
     for cat in adult_cat:
         adult_people += main_db.lookup_by_property("category", cat)
     adult_people_db = Database()
     adult_people_db.add_people(adult_people)
-    joined_after_date_people = adult_people_db.lookup_by_property("date_added", date, comparator=np.greater_equal)
-    
+    joined_after_date_people = adult_people_db.lookup_by_property(
+        "date_added", date, comparator=np.greater_equal
+    )
+
     joined_after_date_db = Database()
     joined_after_date_db.add_people(joined_after_date_people)
     ad_db = AdressDatabase(input_db=joined_after_date_db)
     ad_db.to_excel(output_file)
-    
+
+
 def export_jugend_born_in_year(main_db: Database, year: int, output_file: str):
     jugend_cat = ["Mädchen", "Knaben"]
     jugend_people = []
@@ -64,23 +91,27 @@ def export_jugend_born_in_year(main_db: Database, year: int, output_file: str):
         jugend_people += main_db.lookup_by_property("category", cat)
     jugend_people_db = Database()
     jugend_people_db.add_people(jugend_people)
-    
+
     after_date = pd.Timestamp(str(year))
-    born_after_date_people = jugend_people_db.lookup_by_property("birthday", after_date, comparator=np.greater_equal)
+    born_after_date_people = jugend_people_db.lookup_by_property(
+        "birthday", after_date, comparator=np.greater_equal
+    )
     born_after_date_db = Database()
     born_after_date_db.add_people(born_after_date_people)
-    
-    before_date = pd.Timestamp(str(year+1))
-    born_before_date_people = born_after_date_db.lookup_by_property("birthday", before_date, comparator=np.less)
+
+    before_date = pd.Timestamp(str(year + 1))
+    born_before_date_people = born_after_date_db.lookup_by_property(
+        "birthday", before_date, comparator=np.less
+    )
     born_before_date_db = Database()
     born_before_date_db.add_people(born_before_date_people)
-    
+
     ad_db = AdressDatabase(input_db=born_before_date_db)
     ad_db.to_excel(output_file)
-    
-    
+
+
 path = "/Users/Lukas/Library/CloudStorage/OneDrive-FreigegebeneBibliotheken–TurnvereinWürenlos/Kommunikation - Dokumente/Interne Kommunikation/CleverReach/Adressen/"
-#path = path.replace(" ", "\ ")
+# path = path.replace(" ", "\ ")
 
 fin = "Kontaktliste/Mitglieder12-4_23.xlsx"
 fadditional = "Newsletter_Zusaetzlich.xlsx"
@@ -93,6 +124,9 @@ fout_nomail = "TVW_List_OUT_nomail.xlsx"
 fout_neumitglieder = "TVW_List_OUT_neumitglieder.xlsx"
 fout_jugenduebertritt = "TVW_List_OUT_jugenduebertritte.xlsx"
 
+dc = DynamicsClient()
+fin = dc.download_list_to_folder(path)
+
 main_db = load_main_db(path + fin)
 
 
@@ -104,8 +138,7 @@ mb_db = load_mail_based_db(main_db)
 export_as_cleverreach_csv(mb_db, path + fout)
 export_no_mail_people_excel(mb_db, path + fout_nomail)
 
-#GV
-#export_adult_people_joined_at_or_after_date_excel(main_db, pd.Timestamp('2023-01-15'), path+fout_neumitglieder)
-#export_jugend_born_in_year(main_db, 2007, path+fout_jugenduebertritt)
 
-
+# GV
+export_adult_people_joined_at_or_after_date_excel(main_db, pd.Timestamp('2023-12-01'), path+fout_neumitglieder)
+# export_jugend_born_in_year(main_db, 2007, path+fout_jugenduebertritt)
---
+++ src/utils/dynamics_client.py
@@ -0,0 +1,124 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.firefox.options import Options
+import os
+import shutil
+from onepassword import OnePassword
+
+
+
+
+
+class DynamicsClient:
+    def __init__(self, working_dir: str = None):
+        self._client = None
+        if working_dir is None:
+            working_dir = os.getcwd()
+        self.download_location = os.path.join(working_dir, "dynamics_client_temp_folder")
+        self.create_temporary_download_folder()
+        self.creds = STVAdminCreds()
+        
+    def __del__(self):
+        shutil.rmtree(self.download_location)
+        self.client.close()
+
+    @property
+    def client(self):
+        if self._client is None:        
+            options = Options()
+            options.set_preference("browser.download.folderList", 2)
+            options.set_preference("browser.download.manager.showWhenStarting", False)
+            options.set_preference("browser.download.dir", self.download_location)
+            options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-gzip")
+            self._client = webdriver.Firefox(options=options)
+        return self._client
+    
+    def download_list_to_folder(self,folder):
+        self._login()
+        time.sleep(1)
+        self._initiate_download()
+        while not self.temp_folder_contains_xlsx():
+            pass
+        return self.move_file_to_folder(folder)
+        
+
+    def _login(self):
+        login_user_name = "ctl00$PHM$UserName"
+        login_pwd_name = "ctl00$PHM$Password"
+        login_submit_name = "ctl00$PHM$LoginButton"
+        login_url = "https://nav17.stv-fsg.ch/DynamicsNAV100-NAVUser/WebClient/SignIn.aspx?ReturnUrl=%2fDynamicsNAV100-NAVUser%2fWebClient%2f"
+        self.client.get(login_url)
+        user = self.client.find_element(By.NAME, login_user_name)
+        user.send_keys(self.creds.client_id)
+        pwd = self.client.find_element(By.NAME, login_pwd_name)
+        pwd.send_keys(self.creds.client_secret)
+        self.client.find_element(By.NAME, login_submit_name).click()
+        
+    def _initiate_download(self):
+        self.client.find_element(By.XPATH, "//li[contains(@class, 'ms-cui-tt')]/a[contains(@title, 'Bericht')]").click() #Bericht
+        self.client.find_element(By.XPATH, "//span[@class='ms-cui-ctl-largelabel' and text()='Mitgliederverwaltung']/ancestor::a[@class='ms-cui-ctl-large']").click() #Mitgliederverwaltung
+        time.sleep(3)
+        self.client.find_element(By.XPATH, "//span[@class='ms-cui-ctl-largelabel' and text()='Exportieren']/ancestor::a[@class='ms-cui-ctl-large']").click() #Exportieren
+        time.sleep(1)
+        self.client.find_elements(By.XPATH, "//span[@class='ms-cui-tt-span' and text()='Aktionen']/ancestor::a[@class='ms-cui-tt-a']/ancestor::li[@class='ms-cui-tt ']")[1].click() #Aktionen
+        self.client.find_elements(By.XPATH, "//a[@class='ms-cui-ctl-large' and .//span[@class='ms-cui-ctl-largelabel' and contains(normalize-space(), 'Alle') and contains(normalize-space(), 'auswählen')]]")[0].click() #alle auwählen
+        self.client.find_elements(By.XPATH, "//span[@class='ms-cui-tt-span' and text()='Start']/ancestor::a[@class='ms-cui-tt-a']/ancestor::li[@class='ms-cui-tt ']")[0].click() #Start
+        self.client.find_elements(By.XPATH, "//span[@class='ms-cui-ctl-largelabel' and text()='Exportieren']/ancestor::a[@class='ms-cui-ctl-large']")[1].click() #Exportieren
+
+    def create_temporary_download_folder(self):
+        if os.path.isdir(self.download_location):
+            shutil.rmtree(self.download_location)
+        os.mkdir(self.download_location)
+        
+    def temp_folder_contains_xlsx(self):
+        files = os.listdir(self.download_location)
+        for file in files:
+            if file[-4:]=="xlsx":
+                time.sleep(1)
+                return True
+        return False
+
+    
+    def move_file_to_folder(self, folder):
+        files = os.listdir(self.download_location)
+        files = [file for file in files if "xlsx" in file]
+        assert len(files)==1
+        filename = files[0]
+        os.rename(os.path.join(self.download_location,filename),os.path.join(folder,filename))
+        return filename
+        
+    
+class STVAdminCreds:
+    def __init__(self):
+        self._client_id = None
+        self._client_secret = None
+        self.op = OnePassword()
+        self.item_uuid = "utpuxkadynh4bnoqufqp5umzvy"
+        
+    @property
+    def client_id(self):
+        if self._client_id is None:
+            self._client_id = self.get_client_id()
+        return self._client_id
+    
+    @property
+    def client_secret(self):
+        if self._client_secret is None:
+            self._client_secret = self.get_client_secret()
+        return self._client_secret
+    
+    def get_client_id(self):
+        return self.get_field("username")
+        
+    
+    def get_client_secret(self):
+        return self.get_field("password")
+    
+    def get_field(self, label):
+        item = self.op.get_item(uuid=self.item_uuid)
+        fields = item["fields"]
+        for field in fields:
+            if field["id"] == label:
+                return field["value"]
+                
